2649daf0b6e8b20b670931a54c7e73ddfd2a7776
林京秦 <linjingqin@fline88.com>
2023-09-27 08:37:50 +0800

    add: pressed

--- src/views/editor/CanvasView.vue
+++ src/views/editor/CanvasView.vue
@@ -1,13 +1,15 @@
 <template>
-    <v-stage :config="configKonva">
+    <v-stage :config="configKonva" ref="stageRef" @mousedown="stage.touchStart" @mouseMove="stage.touchMove">
         <v-layer>
-        <v-circle :config="configCircle"></v-circle>
-        <v-rect :config="configRect"></v-rect>
+          <v-rect :config="configSelectionRectangle" :visible="pressed" />
+          <v-circle :config="configCircle"></v-circle>
+          <v-rect :config="configRect"></v-rect>
         </v-layer>
     </v-stage>
 </template>
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
+import { useMousePressed } from '@vueuse/core'
 
 const configKonva = ref({
   width: 900,
@@ -32,4 +34,31 @@ const configRect = ref({
   draggable: true,
 })
 
-  </script>
+const configSelectionRectangle = ref({
+  fill: 'rgba(0,0,255,0.5)',
+  visible: false,
+  width: 0,
+  height: 0,
+  x1: 0,
+  y1: 0,
+  x2: 0,
+  y2: 0
+})
+
+const stageRef = ref(null)
+const { pressed } = useMousePressed({ target: stageRef })
+const stage = {
+  touchStart: (e: MouseEvent) => {
+    console.log(e, '#1')
+    if (e.target !== stageRef.value) return
+
+    // e.evt.preventDefault()
+    configSelectionRectangle.value.visible = true
+    configSelectionRectangle.value.width = 0
+    configSelectionRectangle.value.height = 0
+  },
+  touchMove: (e: MouseEvent) => {
+    console.log(e, '#2')
+  }
+}
+</script>
